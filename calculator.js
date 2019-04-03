32e73535ddd0af8bbba9c19d621a572d5e0a3dbd
Erik <erikcat06@gmail.com>
2019-04-03 14:32:52 -0600

    1/x botón

--- calculator.js
+++ calculator.js
@@ -5,6 +5,7 @@ var botones = document.getElementById('button-addon4');
 botones.innerHTML = `
 <button class="btn btn-danger" id="cube" >x^3</button>
 <button class="btn btn-info" id="x2" >x^2</button>
+<button class="btn btn-secondary" id="inversa" >1/x</button>
 `;
 var cube = document.getElementById('cube');
 cube.addEventListener('click', cube =>
@@ -14,4 +15,8 @@ cube.addEventListener('click', cube =>
 x2.addEventListener('click', x2 =>
 {
       resultado.innerHTML = `<h1>El resultado de ${num.value} <span class="text-info"><strong>x^2</strong></span> es: <span class="alert alert-success">${Math.pow(num.value, 2)}</span></h1>`;
+});
+inversa.addEventListener('click', () =>
+{
+      resultado.innerHTML = `<h1>El resultado de la <span class="text-secondary"><strong>inversa</strong></span> de ${num.value} <span class="text-secondary"><strong>1/x</strong></span> es: <span class="alert alert-success">${1/num.value}</span></h1>`;
 });
